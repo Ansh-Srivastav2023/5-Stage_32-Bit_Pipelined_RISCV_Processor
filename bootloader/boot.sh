4125d2e455e6adb4532871ff1090a28d133e0eee
Ansh-Srivastav2023 <anshs.ec.23@nitj.ac.in>
2026-03-21 09:09:12 +0530

    Remove unused memory files and update Makefiles for improved directory structure
    - Deleted obsolete data memory and instruction memory files from the design/MA directory.
    - Updated the top_module Makefile to use relative paths for GCC and RTL directories.
    - Modified the verilator Makefile to dynamically include all Verilog source files from the top_module and design directories, and added memory files from the memory_files directory.

--- bootloader/boot.sh
+++ bootloader/boot.sh
@@ -4,4 +4,4 @@ riscv-none-elf-objcopy -O binary bootloader.elf bootloader.bin
 
 hexdump -v -e '1/4 "%08x\n"' bootloader.bin > bootloader_rom.hex
 
-mv bootloader_rom.hex /media/anx/New_Volume/Importants/Verilog/open_sta/design/MA
+mv bootloader_rom.hex /media/anx/New_Volume/Importants/Verilog/open_sta/memory_files
